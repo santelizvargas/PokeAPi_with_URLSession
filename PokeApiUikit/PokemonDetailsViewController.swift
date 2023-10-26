ada5967d861f7b15d86d1580dd4c4ab1ac0904e7
santelizvargas <ushija6@gmail.com>
2023-10-26 00:29:03 -0600

    Pokemon detail fixed

--- PokeApiUikit/PokemonDetailsViewController.swift
+++ PokeApiUikit/PokemonDetailsViewController.swift
@@ -7,48 +7,50 @@
 
 import UIKit
 
-class PokemonDetailsViewController: UIViewController {
+final class PokemonDetailsViewController: UIViewController {
+    
+    // MARK: - IBOutlets
+    
+    @IBOutlet private var typePokemon: UILabel!
+    @IBOutlet private var namePokemon: UILabel!
+    @IBOutlet private var imagePokemon: UIImageView!
+    @IBOutlet private var descriptionPokemon: UITextView!
+    @IBOutlet private var attackPokemon: UILabel!
+    @IBOutlet private var defensePokemon: UILabel!
+    @IBOutlet private var stackLevelPokemon: UIStackView!
+    @IBOutlet private var StackDetailsPokeon: UIStackView!
     
     var showPokemon: Pokemon?
-    
-    @IBOutlet weak var typePokemon: UILabel!
-    @IBOutlet weak var namePokemon: UILabel!
-    @IBOutlet weak var imagePokemon: UIImageView!
-    @IBOutlet weak var descriptionPokemon: UITextView!
-    @IBOutlet weak var attackPokemon: UILabel!
-    @IBOutlet weak var defensePokemon: UILabel!
-    
-    @IBOutlet weak var stackLevelPokemon: UIStackView!
-    @IBOutlet weak var StackDetailsPokeon: UIStackView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
+        setupView()
+        imagePokemon.loadFrom(URLAddress: showPokemon?.imageUrl ?? "")
+    }
+    
+    private func setupView() {
         typePokemon.text = "Type \(showPokemon?.type.capitalized ?? "")"
         namePokemon.text = "\(showPokemon?.name.uppercased() ?? "")"
         attackPokemon.text = "Attack: \(showPokemon?.attack ?? 0)"
         defensePokemon.text = "Defense: \(showPokemon?.defense ?? 0)"
         descriptionPokemon.text = showPokemon?.description ?? "this pokemon don't has description"
-        imagePokemon.loadFrom(URLAddress: showPokemon?.imageUrl ?? "")
         
         descriptionPokemon.layer.cornerRadius = 20
         descriptionPokemon.backgroundColor = UIColor(white: 1, alpha: 0.2)
-        
-        
     }
     
 }
 
 extension UIImageView {
+    
     func loadFrom(URLAddress: String) {
+        
         guard let url = URL(string: URLAddress) else { return }
         
         DispatchQueue.main.async {
-            if let imageData = try? Data(contentsOf: url) {
-                if let loadedImage = UIImage(data: imageData) {
-                    self.image = loadedImage
-                }
+            if let imageData = try? Data(contentsOf: url),
+               let loadedImage = UIImage(data: imageData) {
+                self.image = loadedImage
             }
         }
     }
